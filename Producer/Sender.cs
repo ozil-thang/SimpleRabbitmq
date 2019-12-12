95638455e8a9baaaef81f8be3f21d3c613dd9500
Do Thang <dothangbk1998@gmail.com>
2019-12-12 09:47:07 +0700

    test rabbitmq

---
+++ Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string message = "First message netcore rabbitmq";
+                var body = Encoding.UTF8.GetBytes(message);
+                
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Sent message {0}", message);
+            }
+            
+            Console.WriteLine("Press [enter] to exit Sender App");
+            Console.ReadLine();
+        }
+    }
+}
